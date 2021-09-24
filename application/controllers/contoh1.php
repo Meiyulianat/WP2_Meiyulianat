790953303b231cacc5992fedc5c9433820c4b123
meiyulianat <meiyulianat@gmail.com>
2021-09-24 11:59:47 +0700

    Commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h2>Perkenalkan<h2>";
+        echo "Nama Saya Mei Yuliana Tanjung, saya tinggal di bogor";
+    }
+}
