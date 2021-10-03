7ee1763226567d2cc35666d583c56b585d7640db
Meiyulianat <meiyulianat@gmail.com>
2021-10-03 09:31:25 +0700

    Commit-2

--- application/controllers/latihan1.php
+++ application/controllers/latihan1.php
@@ -2,7 +2,7 @@
 class latihan1 extends CI_Controller {
 	public function index()
 	{
-		echo"Hello saya Mei Yuliana Tanjung, selamat belajar WebPro";
+		echo"Hello saya Mei Yuliana Tanjung, selamat belajar WebProII";
 	}
 	public function penjumlahan($n1, $n2)
 	{
--- application/views/view_latihan1.php
+++ application/views/view_latihan1.php
@@ -4,7 +4,7 @@
 	<title>Latihan 1</title>
 </head>
 <body>
-	Hello saya Mei Yuliana Tanjung, selamat belajar WebPro<br>
+	Hello saya Mei Yuliana Tanjung, selamat belajar WebProII<br>
 	Nilai 1 = <?= $nilai1;?>
 	Nilai 2 = <?= $nilai2;?>
 	ini hasil penjumlahan dari metode model yaitu <?= $nilai1 . " + ". $nilai2. " = ".$hasil; ?>
