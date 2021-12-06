98a020bc99ffcc84cad132e82afc004c6b6acb18
Meiyulianat <meiyulianat@gmail.com>
2021-12-06 07:17:06 +0700

    Commit-12

--- application/views/buku/kategori.php
+++ application/views/buku/kategori.php
@@ -30,9 +30,9 @@
                         <td><?= $k['kategori']; ?></td>
                         <td>
                             <a href="<?=
-    base_url('buku/ubahBuku/').$k['id'];?>" class="badge badge-info"><i class="fas fa-edit"></i> Ubah</a>
+    base_url('buku/ubahKategori/').$k['id'];?>" class="badge badge-info"><i class="fas fa-edit"></i> Ubah</a>
                             <a href="<?=
-    base_url('buku/hapusbuku/').$k['id'];?>" onclick="return confirm('Kamu yakin akan menghapus <?= $judul.' '.$k['kategori'];?> ?');" class="badge badge-danger"><i class="fas fa-trash"></i>Hapus</a>
+    base_url('buku/hapusKategori/').$k['id'];?>" onclick="return confirm('Kamu yakin akan menghapus <?= $judul.' '.$k['kategori'];?> ?');" class="badge badge-danger"><i class="fas fa-trash"></i>Hapus</a>
                         </td>
                     </tr>
                     <?php } ?>
@@ -60,15 +60,7 @@
             <form action="<?= base_url('buku/kategori'); ?>" method="post">
                 <div class="modal-body">
                     <div class="form-group">
-                        <select name="kategori" class="form-control  form-control-user">
-                            <option value="">Pilih Kategori</option>
-                            <?php
-                            $k = 
-['Sains','Hobby','Komputer','Komunikasi','Hukum','Agama','Populer','Bahasa','Komik'];
-                            for ($i=0;$i<9;$i++) { ?>
-                                <option value="<?= $k[$i];?>"><?= $k[$i];?></option>
-                            <?php } ?>
-                        </select>
+                    <input type="text" name="kategori" id="kategori" placeholder="Masukan Nama Kategori" class="form-control form_control-user">
                     </div>
                 </div>
                 <div class="modal-footer">
